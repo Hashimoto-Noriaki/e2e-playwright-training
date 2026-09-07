e11b2c15281ab59b5b5ecc015e2c7e8921d934b4
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-09-07 23:26:36 +0900

    動作確認のため10秒間の待機時間とページとブラウザを閉じる操作を追加

--- tests/github-search.ts
+++ tests/github-search.ts
@@ -11,4 +11,8 @@ async function githubSearch(){
     const page = await browser.newPage();
     // GitHubのトップページへ遷移する
     await page.goto("https://github.com");
+    //10秒間の待機時間(通常のテストでは固定のテストの待機は推奨されないが、今回は動作確認のために使用)
+    await page.waitForTimeout(1000);
+    await page.close();
+    await browser.close();
 }
