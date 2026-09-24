c146d54c41f5660df8e4eba42467a7810bae20e9
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-09-25 00:48:22 +0900

    pagesのMainPageの実装

--- tests/testcase/pages/main-page.ts
+++ tests/testcase/pages/main-page.ts
@@ -1,3 +1,16 @@
-export class MainPage {
+import { Locator } from "@playwright/test";
+import { MainPageLocators } from "../locators/main-page-locators";
 
+export class MainPage {
+    async inputTextBox(): Promise<void> {
+        const locator : Locator = this.page.locator(MainPageLocators.TEXT_BOX);
+        await this.page.waitForTimeout(5000)
+        await locator.fill("unittest");
+    }
+
+    async clickGoButton(): Promise<void> {
+        const locator : Locator = this.page.locator(MainPageLocators.GO_BUTTON);
+        await this.page.waitForTimeout(5000)
+        await locator.click();  
+    }
 }
